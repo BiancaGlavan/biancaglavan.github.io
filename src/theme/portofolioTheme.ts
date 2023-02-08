a498ad583fb665f4be4eff6010b27c9de9a1267a
BiancaGlavan <bianca.glavan@yahoo.ro>
2023-02-08 23:33:55 +0200

    created theme

---
+++ src/theme/portofolioTheme.ts
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material/styles";
+
+const portofolioTheme = createTheme({
+  palette: {
+    background: {
+      default: "#212529",
+      paper: "#212529",
+    },
+    primary: {
+        main: "#e9a667",
+    },
+    secondary: {
+        main: "#accecf",
+    },
+    text :{
+      primary: "#fff",
+      secondary: "#cccccc",
+  },
+  },
+  typography: {
+    fontFamily: "Poppins",
+  },
+});
+
+export default portofolioTheme;
